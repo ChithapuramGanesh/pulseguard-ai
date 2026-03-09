14056efe54ac3e32807fcd12eb52f90a78f1ad31
ChithapuramGanesh <chithapuramganesh10@gmail.com>
2026-03-09 22:25:14 +0530

    Delete PulseGuardAI/testingapp.py

--- PulseGuardAI/testingapp.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("dataset/hypertension_dataset.csv")
-
-for col in ["Smoking_Status", "Physical_Activity_Level", "Family_History"]:
-    data[col] = data[col].astype("category")
-    print(col, dict(enumerate(data[col].cat.categories)))
